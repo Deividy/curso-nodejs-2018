2c16c182280faf718ef40afc2ee44fe4cc0df182
Deividy Metheler <deividyz@gmail.com>
2018-03-17 20:21:11 -0300

    add module-4 examples

---
+++ examples/module-4/callback.js
@@ -0,0 +1,25 @@
+function callback() { console.log('hit!'); }
+setTimeout(callback, 1000);
+
+console.log("I'm here!");
+
+function callbackError () { throw new Error('OH NO'); }
+try {
+    setTimeout(callbackError, 1000);
+} catch (ex) {
+    console.log("i will never be hit :/");
+}
+
+function callbackWithNodeErrorPattern (err, result) {
+    if (err) return console.error(err);
+    console.log(result);
+}
+
+setTimeout(() => {
+    callbackWithNodeErrorPattern('OH NO');
+}, 1000);
+
+setTimeout(() => {
+    callbackWithNodeErrorPattern(null, 'Oh good!');
+}, 1000);
+
