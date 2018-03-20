8978a744a316c59131187ab2bda3beca77f22012
Deividy Metheler <deividyz@gmail.com>
2018-03-20 18:20:44 -0300

    add more async / await examples

--- examples/module-4/async-await.js
+++ examples/module-4/async-await.js
@@ -24,6 +24,41 @@ async function readAndProcessFiles () {
 })();
 
 // await readAndProcessFiles();
+class RpgGame {
+    constructor () {
+        this.players = [ ];
+    }
+
+    async loadPlayers () {
+        const players = await readFilePromise('players.txt');
+        this.players = JSON.parse(players.toString());
+    }
+
+    async savePlayers () {
+        const playersString = JSON.stringify(this.players);
+        await writeFilePromise('players.txt', playersString);
+    }
+
+    async addPlayer (playerName) {
+        this.players.push(playerName);
+        await this.savePlayers();
+    }
+}
+
+(async function () {
+    const rpg = new RpgGame();
+
+    try {
+        await rpg.loadPlayers();
+    } catch (ex) {
+        // maybe file is corrupt, we want to keep the flow even if we can't
+        // load the players, so we'll just log here
+        console.error(ex);
+    }
+
+    await rpg.addPlayer(`speedy force member ${new Date().getTime()}`);
+    console.log(rpg.players);
+})();
 
 async function testAsyncWithPromise () {
     return 'COOL';
