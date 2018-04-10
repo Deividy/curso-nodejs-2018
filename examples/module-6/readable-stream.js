02bfe45e47c0e3dd7747a9b96be13b02bd0f43ca
Deividy Metheler <deividyz@gmail.com>
2018-04-10 14:03:17 -0300

    add readable stream example

---
+++ examples/module-6/readable-stream.js
@@ -0,0 +1,34 @@
+const stream = require('stream');
+
+const myResource = "fear is how I fall, confusing what is real";
+class MyReadableStream extends stream.Readable {
+    constructor (options) {
+        super(options);
+        this.readPosition = 0;
+    }
+
+    // we will ignore the size here
+    _read (size) {
+        // setTimeout to simulate something async
+        setTimeout(() => {
+            // here we have to ensure a null return, since node will check by null:
+            // https://github.com/nodejs/node/blob/cf5f986/lib/_stream_readable.js#L228
+            this.push(myResource.split(' ')[this.readPosition++] || null);
+        }, 5 * Math.random()); // since its async, why not use some random time? :)
+    }
+}
+
+
+const myFirstReadableStream = new MyReadableStream();
+
+// parsing this readable stream! :)
+const allWords = [ ];
+myFirstReadableStream.on('data', (data) => {
+    console.log(`Receiving data: ${data}`);
+    allWords.push(data);
+});
+
+myFirstReadableStream.on('end', () => {
+    console.log(' ');
+    console.log(allWords.join(' '));
+});
